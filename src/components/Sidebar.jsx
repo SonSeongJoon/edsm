1b340e6802cf2c283d109195822618b74b301b5b
SoengJoon <sonsj97@gmail.com>
2023-09-19 17:31:23 +0900

    :sparkles: 모바일용 사이드바 css 적용&버튼 클릭시 sider바 들어가게

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import MainButton from "./MainButton";
-import Toggle from "./Toggle";
-import {useNavigate} from "react-router-dom";
+import MainButton from './MainButton';
+import Toggle from './Toggle';
+import { useNavigate } from 'react-router-dom';
 
-export default function Sidebar() {
-	const navigate = useNavigate();
-	const handleOnclick = () => {
-		navigate(`/write`);
-	}
-	return (
-		<div className='w-full flex flex-col  p-5'>
-			<div className='w-full flex justify-center'>
-				<MainButton onclick={handleOnclick}/>
-			</div>
-			<div className='w-full flex mt-5 justify-center'>
-				<Toggle/>
-			</div>
-		</div>
-	);
+export default function Sidebar({toggleSidebar}) {
+  const navigate = useNavigate();
+  const handleOnclick = () => {
+    navigate(`/write`);
+  };
+  return (
+    <div className="w-full flex flex-col  p-5">
+      <div className="w-full flex justify-center">
+        <MainButton onclick={handleOnclick} />
+      </div>
+      <div className="w-full flex mt-5 justify-center">
+         <Toggle toggleSidebar={toggleSidebar} />
+      </div>
+    </div>
+  );
 }
--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -9,7 +9,7 @@ const list = [
   { key: 4, name: '수신함', path: 'receive' },
 ];
 
-export default function Toggle() {
+export default function Toggle({toggleSidebar}) {
   const navigate = useNavigate();
   const location = useLocation();
   const {user} = useAuthContext();
@@ -17,32 +17,35 @@ export default function Toggle() {
     navigate(`/${path}`);
   };
 
-  return (
-    <div className="w-[200px]">
-      <div className="flex items-center">
-        <h3 className="text-md sm:text-lg font-bold">결재 문서</h3>
-      </div>
-       <ul className="mt-1">
-          {list.map((item) => {
-             if (item.name === '수신함' && (!user || !user.isAdmin)) {
-                return null;
-             }
-             return (
-                <li
-                   key={item.key}
-                   className={`py-2 px-1 text-sm sm:text-md rounded-lg cursor-pointer 
+   return (
+      <div className="w-[200px]">
+         <div className="flex items-center">
+            <h3 className="text-md sm:text-lg font-bold">결재 문서</h3>
+         </div>
+         <ul className="mt-1">
+            {list.map((item) => {
+               if (item.name === '수신함' && (!user || !user.isAdmin)) {
+                  return null;
+               }
+               return (
+                  <li
+                     key={item.key}
+                     className={`py-2 px-1 text-sm sm:text-md rounded-lg cursor-pointer 
                           ${
-                      location.pathname.includes(item.path)
-                         ? 'bg-brand opacity-60 text-white'
-                         : 'hover:bg-brand hover:bg-opacity-20 hover:text-black'
-                   }`}
-                   onClick={() => handleClick(item.path)}
-                >
-                   {item.name}
-                </li>
-             );
-          })}
-       </ul>
-    </div>
-  );
+                        location.pathname.includes(item.path)
+                           ? 'bg-brand opacity-60 text-white'
+                           : 'hover:bg-brand hover:bg-opacity-20 hover:text-black'
+                     }`}
+                     onClick={() => {
+                        handleClick(item.path);
+                        toggleSidebar();
+                     }}
+                  >
+                     {item.name}
+                  </li>
+               );
+            })}
+         </ul>
+      </div>
+   );
 }
