61481398c61fe188fc6b5eb0e6e5a38abb07be96
SoengJoon <sonsj97@gmail.com>
2023-09-22 22:06:28 +0900

    :sparkles: 반려사유 각 card 마진

--- src/components/ReasonText.jsx
+++ src/components/ReasonText.jsx
@@ -4,7 +4,7 @@ export default function ReasonText({ reasonText }) {
       <h1 className="font-bold text-xl mb-1">반려사유</h1>
       {reasonText &&
         Object.entries(reasonText).map(([key, value]) => (
-          <div key={key} className="p-4 border rounded-md shadow-sm bg-white">
+          <div key={key} className="p-4 border rounded-md shadow-sm bg-white mb-2">
             <h5 className="font-semibold mb-2">{key}</h5>
             <p>{value}</p>
           </div>
