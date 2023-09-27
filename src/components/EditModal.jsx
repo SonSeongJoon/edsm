7ec109b69329c825d9bce75c8a6bdb3e6e533a36
SoengJoon <sonsj97@gmail.com>
2023-09-27 11:25:30 +0900

    :sparkles: detail - 품의서 수정 컴포넌트 생성

--- src/components/EditModal.jsx
+++ src/components/EditModal.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
-import ModalExpend from "./html/ModalExpend";
-import ModalVacation from "./html/Modalvacation";
+import ModalExpend from './html/ModalExpend';
+import ModalVacation from './html/Modalvacation';
+import Modalapproval from './html/Modalapproval';
 
 export function EditModal({
   modalProduct,
@@ -39,42 +40,45 @@ export function EditModal({
   };
 
   return (
-     <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center text-xs sm:text-sm">
-       <div className="bg-white p-2 sm:p-5 rounded shadow-lg w-[360px] sm:w-[800px] max-h-[80vh] sm:max-h-[500px] overflow-y-auto border border-gray-500">
-         <h1 className="text-xl sm:text-3xl font-bold mb-3">수정하기</h1>
+    <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center text-xs sm:text-sm">
+      <div className="bg-white p-2 sm:p-5 rounded shadow-lg w-[360px] sm:w-[800px] max-h-[80vh] sm:max-h-[500px] overflow-y-auto border border-gray-500">
+        <h1 className="text-xl sm:text-3xl font-bold mb-3">수정하기</h1>
 
-         {modalProduct.file === '지출결의서' ? (
-            <ModalExpend
-               modalProduct={modalProduct}
-               handleEditChange={handleEditChange}
-               handleItemValue={handleItemValue}
-               handleRemoveItem={handleRemoveItem}
-               handleAddItem={handleAddItem}
-            />
-         ) : modalProduct.file === '휴가계' ? (
-            <ModalVacation
-               modalProduct={modalProduct}
-               handleEditChange={handleEditChange}
-               // 다른 props들도 필요하다면 전달합니다.
-            />
-         ) : null}
+        {modalProduct.file === '지출결의서' ? (
+          <ModalExpend
+            modalProduct={modalProduct}
+            handleEditChange={handleEditChange}
+            handleItemValue={handleItemValue}
+            handleRemoveItem={handleRemoveItem}
+            handleAddItem={handleAddItem}
+          />
+        ) : modalProduct.file === '휴가계' ? (
+          <ModalVacation
+            modalProduct={modalProduct}
+            handleEditChange={handleEditChange}
+          />
+        ) : modalProduct.file === '품의서' ? (
+          <Modalapproval
+            modalProduct={modalProduct}
+            handleEditChange={handleEditChange}
+          />
+        ) : null}
 
-         <div className="flex w-full justify-center mt-3">
-           <button
-              className="border px-1 py-1 rounded-md bg-gray-100 mr-1"
-              onClick={handleSave}
-           >
-             저장
-           </button>
-           <button
-              className="border px-1 py-1 rounded-md bg-gray-100"
-              onClick={closeEditModal}
-           >
-             닫기
-           </button>
-         </div>
-       </div>
-     </div>
+        <div className="flex w-full justify-center mt-3">
+          <button
+            className="border px-1 py-1 rounded-md bg-gray-100 mr-1"
+            onClick={handleSave}
+          >
+            저장
+          </button>
+          <button
+            className="border px-1 py-1 rounded-md bg-gray-100"
+            onClick={closeEditModal}
+          >
+            닫기
+          </button>
+        </div>
+      </div>
+    </div>
   );
-
 }
