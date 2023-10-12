068001794ee79e23a9e407f070e3cac23e3ade49
SoengJoon <sonsj97@gmail.com>
2023-10-12 15:08:27 +0900

    :sparkles: 폼의서 detail 단 구성 작업

--- src/components/html/approvalShow.jsx
+++ src/components/html/approvalShow.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const ApprovalShow = ({ product }) => {
-	const { title, file, date, dept, detail, name } = product;
+	const { title, content, period, price, note, dept, name, file } = product;
+	const date = new Date().toLocaleDateString('ko-KR', { year: 'numeric', month: '2-digit', day: '2-digit' }).replace(/. /g, '.');
 
 	return (
 		<div>
@@ -20,10 +21,30 @@ const ApprovalShow = ({ product }) => {
 					<h1 className="text-gray-600 text-xm sm:text-md">{date}</h1>
 				</div>
 			</div>
-			<div className="flex flex-col header space-y-4 md:space-y-6 lg:space-y-8">
-				<div className="flex items-center">
-					<h1 className="font-bold mr-2 sm:text-md text-xm">상세 내용 : </h1>
-					<p className="text-gray-600 text-sm sm:text-md">{detail}</p>
+			<div className='border p-2 rounded-lg border-gray-300'>
+				<div className="flex flex-col">
+						<div className='flex'>
+							<h1 className="font-bold mr-2 sm:text-md text-xm">
+								항 목 :{' '}
+							</h1>
+							<p className="text-gray-600 text-sm sm:text-md">
+								{content}
+							</p>
+						</div>
+						<div className="flex items-center mt-1">
+							<h1 className="font-bold mr-2 sm:text-md text-xm">
+								기 간 :{' '}
+							</h1>
+							<p className="text-gray-600 text-sm sm:text-md">{period}</p>
+						</div>
+						<div className="flex items-center mt-1">
+							<h1 className="font-bold mr-2 sm:text-md text-xm">금 액 : </h1>
+							<p className="text-gray-600 text-sm sm:text-md">{price}</p>
+						</div>
+						<div className="flex items-center mt-1">
+							<h1 className="font-bold mr-2 sm:text-md text-xm">비 고 : </h1>
+							<p className="text-gray-600 text-sm sm:text-md">{note}</p>
+						</div>
 				</div>
 			</div>
 		</div>
