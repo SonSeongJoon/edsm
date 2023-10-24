38a0a72b7649fe2837595a5de034dabb47608ba4
SoengJoon <sonsj97@gmail.com>
2023-10-24 10:55:05 +0900

    :sparkles: 상태정보 필터 모두 적용

--- src/components/TableComponent.jsx
+++ src/components/TableComponent.jsx
@@ -181,20 +181,16 @@ export const TableComponent = ({
         <thead>
           <tr>
             <th className="px-3 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              {isAdmins || !isMst ? (
-                '읽음표시'
-              ) : (
-                <select
-                  onChange={(e) => setSelectState(e.target.value)}
-                  className="bg-transparent focus:outline-none focus:ring-2 focus:ring-indigo-400"
-                >
-                  {uniqueState.map((state) => (
-                    <option key={state} value={state}>
-                      {state}
-                    </option>
-                  ))}
-                </select>
-              )}
+              <select
+                 onChange={(e) => setSelectState(e.target.value)}
+                 className="bg-transparent focus:outline-none focus:ring-2 focus:ring-indigo-400"
+              >
+                {uniqueState.map((state) => (
+                   <option key={state} value={state}>
+                     {state}
+                   </option>
+                ))}
+              </select>
             </th>
             <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
               제목
