7f8d046365cc482b7f0b3531c42da1eb04a2e1da
SoengJoon <sonsj97@gmail.com>
2023-10-27 02:21:29 +0900

    :sparkles: 필요없는 부분 제거

--- src/components/PaperList.jsx
+++ src/components/PaperList.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import { useQuery } from '@tanstack/react-query';
-import { useLocation } from 'react-router-dom';
 import { getAll, getProduct, getReceive } from '../api/firebase';
 import { TableComponent } from './TableComponent';
 
 export default function PaperList({ category, state, adminData, MstData }) {
-  const location = useLocation();
-  const basePath = location.pathname.split('/')[1];
 
-  // queryKey 및 queryFunction 구성 로직
   let queryKey;
   let queryFunction;
   if (!adminData && !MstData) {
