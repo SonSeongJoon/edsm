adada4af7269685bb41bcc6c47cdfde922a8f10b
SoengJoon <sonsj97@gmail.com>
2023-11-01 16:11:27 +0900

    :sparkles: 수신함 디자인 변경

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,84 +5,89 @@ import { getPersonalData, getPersonalReceive } from '../api/firebase';
 import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
-	const { user } = useAuthContext();
-	const isAdmin = user?.isAdmin;
-	const userId = user?.uid;
-	const navigate = useNavigate();
+  const { user } = useAuthContext();
+  const isAdmin = user?.isAdmin;
+  const userId = user?.uid;
+  const navigate = useNavigate();
 
+  const {
+    isLoading: isLoadingPersonalData,
+    error: errorPersonalData,
+    data: personalData,
+  } = useQuery(['data'], () => getPersonalData(userId), { enabled: !!userId });
 
-	const {
-		isLoading: isLoadingPersonalData,
-		error: errorPersonalData,
-		data: personalData,
-	} = useQuery(['data'], () => getPersonalData(userId), { enabled: !!userId });
+  const {
+    isLoading: isLoadingReceivedData,
+    error: errorReceivedData,
+    data: receivedData,
+  } = useQuery(['receivedData'], () => getPersonalReceive(userId), { enabled: isAdmin });
 
-	const {
-		isLoading: isLoadingReceivedData,
-		error: errorReceivedData,
-		data: receivedData,
-	} = useQuery(['receivedData'], () => getPersonalReceive(userId), { enabled: isAdmin });
+  if (isLoadingPersonalData || (isAdmin && isLoadingReceivedData)) {
+    return (
+      <div className="h-full flex justify-center items-center">
+        <span>Loading...</span>
+      </div>
+    );
+  }
 
-	if (isLoadingPersonalData || (isAdmin && isLoadingReceivedData)) {
-		return (
-			<div className="h-full flex justify-center items-center">
-				<span>Loading...</span>
-			</div>
-		);
-	}
+  if (errorPersonalData || (isAdmin && errorReceivedData)) {
+    return (
+      <div className="h-full flex justify-center items-center">
+        <span>Error: {(errorPersonalData || errorReceivedData).message}</span>
+      </div>
+    );
+  }
 
-	if (errorPersonalData || (isAdmin && errorReceivedData)) {
-		return (
-			<div className="h-full flex justify-center items-center">
-				<span>Error: {(errorPersonalData || errorReceivedData).message}</span>
-			</div>
-		);
-	}
+  const currentItems = personalData ? personalData : [];
+  const receivedItems = receivedData ? receivedData : [];
 
-	const currentItems = personalData ? personalData : [];
-	const receivedItems = receivedData ? receivedData : [];
+  const getStatusCounts = (items) =>
+    items.reduce((acc, state) => {
+      acc[state] = (acc[state] || 0) + 1;
+      return acc;
+    }, {});
 
-	const getStatusCounts = (items) => items.reduce((acc, state) => {
-		acc[state] = (acc[state] || 0) + 1;
-		return acc;
-	}, {});
+  const personalStatusCounts = getStatusCounts(currentItems);
+  const receivedStatusCounts = getStatusCounts(receivedItems);
+  const statuses = ['대기', '반려', '승인'];
 
-	const personalStatusCounts = getStatusCounts(currentItems);
-	const receivedStatusCounts = getStatusCounts(receivedItems);
-	const statuses = ['대기', '반려', '승인'];
-
-	return (
-		<div className="h-full flex flex-col justify-center items-center bg-gray-50 py-10">
-			<h1 className="font-bold text-2xl mb-5 text-gray-700">결재 현황</h1>
-			<div className="grid grid-cols-3 gap-4 mb-6 w-full max-w-md">
-				{statuses.map((status) => (
-					<div key={status}
-					     className="flex flex-col items-center justify-center bg-white border border-gray-300 rounded-lg p-4 shadow-md cursor-pointer transition transform hover:scale-105"
-					     onClick={() => {
-						     if (status === '대기') navigate('/wait');
-						     if (status === '반려') navigate('/reject');
-						     if (status === '승인') navigate('/complete');
-					     }}>
-						<span className="text-lg font-bold text-indigo-500">{status}</span>
-						<span className="text-gray-600">{personalStatusCounts[status] || 0}건</span>
-					</div>
-				))}
-			</div>
-			{isAdmin && (
-				<>
-					<h1 className="font-bold text-2xl mb-5 text-gray-700">수신함 현황</h1>
-					<div className="grid grid-cols-1 gap-4 mb-6 w-full max-w-md">
-						<div className="flex flex-col items-center justify-center bg-white border border-gray-300 rounded-lg p-4 shadow-md cursor-pointer transition transform hover:scale-105"
-						     onClick={() => navigate('/receive')}>
-							<span className="text-lg font-bold text-indigo-500 mb-1">결재를 기다리고 있습니다</span>
-							<span className="text-gray-600">{receivedStatusCounts['대기'] || 0}건</span>
-						</div>
-					</div>
-				</>
-			)}
-			<span className="text-md mb-5 text-gray-400">카드를 클릭하면 페이지 이동됩니다.</span>
-			<img src="/seoulir.png" alt="seoulir" className="w-[200px]"/>
-		</div>
-	);
-
+  return (
+    <div className="h-full flex flex-col justify-center items-center bg-gray-50 py-10">
+      <h1 className="font-bold text-2xl mb-5 text-gray-700">결재 현황</h1>
+      <div className="grid grid-cols-3 gap-4 mb-6 w-full max-w-md">
+        {statuses.map((status) => (
+          <div
+            key={status}
+            className="flex flex-col items-center justify-center bg-white border border-gray-300 rounded-lg p-4 shadow-md cursor-pointer transition transform hover:scale-105"
+            onClick={() => {
+              if (status === '대기') navigate('/wait');
+              if (status === '반려') navigate('/reject');
+              if (status === '승인') navigate('/complete');
+            }}
+          >
+            <span className="text-lg font-bold text-indigo-500">{status}</span>
+            <span className="text-gray-600">{personalStatusCounts[status] || 0}건</span>
+          </div>
+        ))}
+      </div>
+      {isAdmin && (
+        <>
+          <h1 className="font-bold text-2xl mb-5 text-gray-700">수신함 현황</h1>
+          <div className="grid grid-cols-1 gap-4 mb-6 w-full max-w-md">
+            <div
+              className="flex flex-col items-center justify-center bg-white border border-gray-300 rounded-lg p-4 shadow-md cursor-pointer transition transform hover:scale-105"
+              onClick={() => navigate('/receive')}
+            >
+              <span className="flex text-lg items-end font-bold">
+                <span className="text-indigo-500 mr-2">{receivedStatusCounts['대기'] || 0}</span>
+                <p className="text-gray-600 text-md font-medium">건의 결재가 아직 남아있습니다.</p>
+              </span>
+            </div>
+          </div>
+        </>
+      )}
+      <span className="text-md mb-5 text-gray-400">카드를 클릭하면 페이지 이동됩니다.</span>
+      <img src="/seoulir.png" alt="seoulir" className="w-[200px]" />
+    </div>
+  );
 }
