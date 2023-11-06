8dda2c4895727d53721fa1dc21959bb036c0ed22
SoengJoon <sonsj97@gmail.com>
2023-11-06 14:53:26 +0900

    :sparkles: 휴가계 form 반차선택시 문구표시

--- src/components/form/VacationForm.jsx
+++ src/components/form/VacationForm.jsx
@@ -23,9 +23,7 @@ const initVacationForm = {
 };
 
 const VacationForm = ({ product, handleChange }) => {
-  const [vacations, setVacations] = useState(
-    product?.Vacations || initVacationForm.Vacations,
-  );
+  const [vacations, setVacations] = useState(product?.Vacations || initVacationForm.Vacations);
 
   const addVacationPeriod = () => {
     if (product.Vacations?.length < 3) {
@@ -68,7 +66,6 @@ const VacationForm = ({ product, handleChange }) => {
     handleChange({ target: { name: 'Vacations', value: newVacations } });
   };
 
-
   const toggleHalfDay = (index) => {
     const newVacations = [...vacations];
     if (newVacations[index].daysDifference === '1' || newVacations[index].daysDifference === '0.5') {
@@ -79,7 +76,6 @@ const VacationForm = ({ product, handleChange }) => {
       handleChange({ target: { name: 'Vacations', value: newVacations } });
     }
   };
-
 
   useEffect(() => {
     const totalLeaveDays = parseFloat(product.TotalLeaveDays) || 0.0;
@@ -95,196 +91,160 @@ const VacationForm = ({ product, handleChange }) => {
     handleChange({ target: { name: 'Vacations', value: vacations } });
   }, [vacations, handleChange]);
 
-
   return (
-    <div className="max-w-screen-lg mx-auto">
-      <div className="container bg-white p-2">
-        <div className="mb-3">
-          <label
-            className="block sm:text-md text-xm font-bold mb-2"
-            htmlFor="title"
-          >
+    <div className='max-w-screen-lg mx-auto'>
+      <div className='container bg-white p-2'>
+        <div className='mb-3'>
+          <label className='block sm:text-md text-xm font-bold mb-2' htmlFor='title'>
             제목:
           </label>
           <input
-            type="text"
-            name="title"
-            id="title"
-            placeholder="제목 입력"
+            type='text'
+            name='title'
+            id='title'
+            placeholder='제목 입력'
             value={product.title || ''}
-            className="w-[250px] px-3 py-2 border rounded shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
+            className='w-[250px] px-3 py-2 border rounded shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500'
             onChange={handleChange}
           />
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-5">
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mb-5'>
           <div>
-            <label
-              className="block sm:text-md text-xm font-bold mb-2"
-              htmlFor="AttributionYear"
-            >
+            <label className='block sm:text-md text-xm font-bold mb-2' htmlFor='AttributionYear'>
               귀속연도:
             </label>
             <input
-              type="number"
-              name="AttributionYear"
-              id="AttributionYear"
-              placeholder="귀속연도 입력"
+              type='number'
+              name='AttributionYear'
+              id='AttributionYear'
+              placeholder='귀속연도 입력'
               value={product.AttributionYear || ''}
-              className="w-[250px] px-3 py-2 border rounded shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
+              className='w-[250px] px-3 py-2 border rounded shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500'
               onChange={handleChange}
             />
           </div>
         </div>
-        <div className="flex space-x-2 sm:space-x-2">
+        <div className='flex space-x-2 sm:space-x-2'>
           <div>
-            <label
-              className="font-bold sm:text-md text-xm mr-2"
-              htmlFor="TotalLeaveDays"
-            >
+            <label className='font-bold sm:text-md text-xm mr-2' htmlFor='TotalLeaveDays'>
               총 연차일수
             </label>
             <input
-              type="text"
-              pattern="\d*\.?\d*" // allows numbers and a single decimal point
-              name="TotalLeaveDays"
-              id="TotalLeaveDays"
+              type='text'
+              pattern='\d*\.?\d*' // allows numbers and a single decimal point
+              name='TotalLeaveDays'
+              id='TotalLeaveDays'
               value={product.TotalLeaveDays || ''}
-              className="w-20 px-3 py-2 border rounded shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
+              className='w-20 px-3 py-2 border rounded shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500'
               onChange={handleChange}
             />
           </div>
           <div>
-            <label
-              className="font-bold sm:text-md text-xm mr-2"
-              htmlFor="UsedDays"
-            >
+            <label className='font-bold sm:text-md text-xm mr-2' htmlFor='UsedDays'>
               기사용일수
             </label>
             <input
-              type="text"
-              pattern="\d*\.?\d*" // allows numbers and a single decimal point
-              name="UsedDays"
-              id="UsedDays"
+              type='text'
+              pattern='\d*\.?\d*' // allows numbers and a single decimal point
+              name='UsedDays'
+              id='UsedDays'
               value={product.UsedDays || ''}
-              className="w-20 px-3 py-2 border rounded shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
+              className='w-20 px-3 py-2 border rounded shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500'
               onChange={handleChange}
             />
           </div>
           <div>
-            <label
-              className="font-bold sm:text-md text-xm mr-2"
-              htmlFor="RemainDays"
-            >
+            <label className='font-bold sm:text-md text-xm mr-2' htmlFor='RemainDays'>
               잔여일수
             </label>
             <input
-              type="text"
-              name="RemainDays"
-              id="RemainDays"
+              type='text'
+              name='RemainDays'
+              id='RemainDays'
               value={product.RemainDays || ''}
               readOnly={true}
-              className="w-20 px-3 py-2 border rounded shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
+              className='w-20 px-3 py-2 border rounded shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500'
               onChange={handleChange}
             />
           </div>
         </div>
 
-        <div className="mt-5">
+        <div className='mt-5'>
           {product?.Vacations &&
             product.Vacations.map((vacation, index) => (
-              <div key={index} className="mb-4 ">
+              <div key={index} className='mb-4 '>
                 <div>
-                  <div className="flex-col border sm:p-5 p-2 rounded-lg shadow-lg text-xm sm:text-md">
-                    <div className="flex space-x-2 items-center">
+                  <div className='flex-col border sm:p-5 p-2 rounded-lg shadow-lg text-xm sm:text-md'>
+                    <div className='flex space-x-2 items-center'>
                       <div className='flex items-center'>
-                        <label
-                          className="font-bold sm:text-md text-xm mr-2"
-                          htmlFor={`startDate${index}`}
-                        >
+                        <label className='font-bold sm:text-md text-xm mr-2' htmlFor={`startDate${index}`}>
                           시작
                         </label>
                         <input
-                          type="date"
+                          type='date'
                           name={`startDate${index}`}
                           value={vacation.startDate}
-                          className="sm:px-3 sm:py-2 px-1 py-2 border rounded shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
-                          onChange={(e) =>
-                            handleVacationChange(
-                              index,
-                              'startDate',
-                              e.target.value,
-                            )
-                          }
+                          className='sm:px-3 sm:py-2 px-1 py-2 border rounded shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500'
+                          onChange={(e) => handleVacationChange(index, 'startDate', e.target.value)}
                         />
                       </div>
                       <div className='flex items-center'>
-                        <label
-                          className="font-bold sm:text-md text-xm mr-2"
-                          htmlFor={`endDate${index}`}
-                        >
+                        <label className='font-bold sm:text-md text-xm mr-2' htmlFor={`endDate${index}`}>
                           종료
                         </label>
                         <input
-                          type="date"
+                          type='date'
                           name={`endDate${index}`}
                           value={vacation.endDate}
-                          className="sm:px-3 sm:py-2 px-1 py-2 border rounded shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
-                          onChange={(e) =>
-                            handleVacationChange(
-                              index,
-                              'endDate',
-                              e.target.value,
-                            )
-                          }
+                          className='sm:px-3 sm:py-2 px-1 py-2 border rounded shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500'
+                          onChange={(e) => handleVacationChange(index, 'endDate', e.target.value)}
                         />
-                        <div className="px-2 py-1 rounded font-bold text-green-700 text-xm sm:text-sm">
+                        <div className='px-2 py-1 rounded font-bold text-green-700 text-xm sm:text-sm'>
                           {vacation.daysDifference} 일간
                         </div>
                       </div>
-
-                      {(vacation.daysDifference === '1' ||
-                        vacation.daysDifference === '0.5') && (
-                        <button
-                          onClick={() => toggleHalfDay(index)}
-                          className="ml-2 px-2 py-1 border rounded bg-gray-500 text-white text-xm"
-                        >
-                          {vacation.isHalfDay ? '취소' : '반차'}
-                        </button>
-                      )}
+                      <div>
+                        {(vacation.daysDifference === '1' || vacation.daysDifference === '0.5') && (
+                          <button
+                            onClick={() => toggleHalfDay(index)}
+                            className='ml-2 px-2 py-1 border rounded bg-gray-500 text-white text-xm'
+                          >
+                            {vacation.isHalfDay ? '취소' : '반차'}
+                          </button>
+                        )}
+                      </div>
                     </div>
-                    <div className="mt-2 flex items-center">
-                      <label
-                         className="block sm:text-md text-xm font-bold mr-2"
-                         htmlFor={`vacationReason${index}`}
-                      >
-                        휴가사유
-                      </label>
-                      <input
-                         type="text"
-                         name={`vacationReason${index}`}
-                         placeholder="휴가사유 입력"
-                         value={vacation.vacationReason}
-                         className="w-1/2 px-2 py-1 border rounded shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
-                         onChange={(e) =>
-                            handleVacationChange(index, 'vacationReason', e.target.value)
-                         }
-                      />
+                    <div className='mt-1 flex-col '>
+                      <div className='text-sky-500 text-xm sm:text-sm mb-2'>{vacation.isHalfDay ? '오전/오후 반차 표시를 제목에 작성해주세요!' : null}</div>
+                      <div className='flex items-center'>
+                        <label className='block sm:text-md text-xm font-bold mr-2' htmlFor={`vacationReason${index}`}>
+                          휴가사유
+                        </label>
+                        <input
+                          type='text'
+                          name={`vacationReason${index}`}
+                          placeholder='휴가사유 입력'
+                          value={vacation.vacationReason}
+                          className='w-1/2 px-2 py-1 border rounded shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500'
+                          onChange={(e) => handleVacationChange(index, 'vacationReason', e.target.value)}
+                        />
+                      </div>
                     </div>
                   </div>
-                  <div className="mt-2 flex justify-end">
+                  <div className='mt-2 flex justify-end'>
                     {index === product.Vacations.length - 1 && (
                       <div>
                         {index !== 0 && (
                           <button
                             onClick={() => removeVacationPeriod(index)}
-                            className="ml-2 px-3 py-1 border rounded bg-brand text-white text-xm"
+                            className='ml-2 px-3 py-1 border rounded bg-brand text-white text-xm'
                           >
                             삭제
                           </button>
                         )}
                         <button
                           onClick={addVacationPeriod}
-                          className="ml-2 px-3 py-1 border rounded bg-blue-600 text-white text-xm"
+                          className='ml-2 px-3 py-1 border rounded bg-blue-600 text-white text-xm'
                         >
                           휴가 추가
                         </button>
@@ -301,4 +261,4 @@ const VacationForm = ({ product, handleChange }) => {
 };
 
 export default VacationForm;
-export {initVacationForm};
+export { initVacationForm };
